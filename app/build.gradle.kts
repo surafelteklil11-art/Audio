7dd0dfe91247a9847dbdc63e6192b0aa9fefc686
surafelteklil11-art <surafelteklil11@gmail.com>
2026-08-11 08:44:03 +0300

    Add RecyclerView dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 android {
     namespace = "com.surafel.audio"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.surafel.audio"
         minSdk = 24
@@ -20,6 +19,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.media3:media3-exoplayer:1.5.1")
     implementation("androidx.media3:media3-session:1.5.1")
